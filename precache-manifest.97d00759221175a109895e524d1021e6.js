34e22e0dcb407f52ba69899d0bc2e3a3a73c44a8
Amanda Claproth <amanda.claproth@gmail.com>
2020-10-16 13:50:10 -0700

    Updates

---
+++ precache-manifest.97d00759221175a109895e524d1021e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e916bfdd571e35b4d5e022394f2df5",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "a6a23959e8ffd8b43134",
+    "url": "/project-4-assessment/static/css/main.c0835aaf.chunk.css"
+  },
+  {
+    "revision": "7f8d0bc52e06c661cf26",
+    "url": "/project-4-assessment/static/js/2.a74b166f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.a74b166f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a23959e8ffd8b43134",
+    "url": "/project-4-assessment/static/js/main.738f4acf.chunk.js"
+  },
+  {
+    "revision": "65095c8e39bb9eb076dc",
+    "url": "/project-4-assessment/static/js/runtime-main.27ac797a.js"
+  }
+]);
